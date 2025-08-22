d7b2703f95fbfd58b94dd5addb1e76c261e9c042
pejowe33 <160105603+pejowe33@users.noreply.github.com>
2025-08-22 17:26:35 -0500

    Add .next/static/chunks/src_app_supabase-test_page_tsx_a993527f._.js 1755901595447

---
+++ .next/static/chunks/src_app_supabase-test_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_c0e23055.js",
+  "static/chunks/src_55bc92a4._.js",
+  "static/chunks/node_modules_932792be._.js"
+],
+    source: "dynamic"
+});
